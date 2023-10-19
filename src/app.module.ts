c80e2015ad8c62ebd63b764881c2a07b19adbe16
Jakub Tkaczyk <mowmikubek@gmail.com>
2023-10-19 09:41:49 +0200

    Rebind SerializeInterceptor to app module. Update JWT expiration time

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { EventsModule } from './events/events.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { CostsModule } from './costs/costs.module';
 import databaseConfig from '../database/database.config';
+import { SerializeInterceptor } from './interceptors/serialize.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -29,6 +31,13 @@ import databaseConfig from '../database/database.config';
 
     ],
     controllers: [AppController],
-    providers: [AppService, ConfigService],
+    providers: [
+        AppService, 
+        ConfigService,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: SerializeInterceptor,
+        }
+    ],
 })
 export class AppModule {}
